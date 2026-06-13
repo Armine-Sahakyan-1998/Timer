49c99a1980e88f6fefba1cbc0458a34b46ed8aba
Armine-Sahakyan-1998 <arm1998.sahakyan@mail.ru>
2026-06-13 10:44:45 +0400

    Add files via upload

---
+++ timer.py
@@ -0,0 +1,20 @@
+import time
+def second_func():
+            while True:
+                print("\nThe second_function game")
+                print("1. Start")
+                print("2. Exit")
+                choose = input("Choose your number (1 or 2)")
+                if choose == '1':
+                    start_time = time.time()
+                    input("You are started a game,please click the 'Enter' butoon for the end!")
+
+                    end_time = time.time()
+                    tevox = end_time - start_time
+                    print(f"Ancac zhamanakn e {tevox} vayrkyan")
+                elif choose == '2':
+                    print("The programm is ending")
+                    break
+                else:
+                    print("Invalid choice.Please enter 1 or 2!")
+second_func()
